6e928cc0a0456a385e7ae5221e692ab9b8dbbc97
Xingqang Bai <bxq2011hust@qq.com>
2020-05-21 19:56:46 +0800

    fix sometimes sm2 signature length!=128 (#15)

--- .ci/integration_test.sh
+++ .ci/integration_test.sh
@@ -182,7 +182,7 @@ integration_std()
     execute_cmd "./abigen --bin .ci/hello/HelloWorld.bin --abi .ci/hello/HelloWorld.abi  --type Hello --pkg main --out=hello.go"
     generate_hello Hello hello.go
     execute_cmd "go build -o hello hello.go"
-    execute_cmd "go build -o bn256 .ci/Precompiledbn256/bn256.go"
+    execute_cmd "go build -o bn256 .ci/ethPrecompiled/bn256.go"
     LOG_INFO "generate hello.go and build hello done."
 
     bash build_chain.sh -l 127.0.0.1:4 -o nodes
@@ -212,7 +212,7 @@ integration_gm()
     execute_cmd "./abigen --bin .ci/hello/HelloWorld_gm.bin --abi .ci/hello/HelloWorld.abi  --type Hello --pkg main --out=hello_gm.go --smcrypto=true"
     generate_hello Hello hello_gm.go
     execute_cmd "go build -o hello_gm hello_gm.go"
-    execute_cmd "go build -o bn256_gm .ci/Precompiledbn256/bn256_gm.go"
+    execute_cmd "go build -o bn256_gm .ci/ethPrecompiled/bn256_gm.go"
     LOG_INFO "generate hello_gm.go and build hello_gm done."
 
     bash build_chain.sh -l 127.0.0.1:4 -g -o nodes_gm
