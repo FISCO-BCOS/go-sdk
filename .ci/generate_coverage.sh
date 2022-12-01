8d096eb1f865522ab041d7a024c9e85a05f22882
ylxiao5 <ylxiao5@iflytek.com>
2022-12-01 14:20:59 +0800

    fix bug

--- .ci/generate_coverage.sh
+++ .ci/generate_coverage.sh
@@ -14,7 +14,7 @@ LOG_INFO() {
 
 calculate_coverage() {
     # start blockchain demo
-    latest_version=$(curl -sS https://gitee.com/api/v5/repos/FISCO-BCOS/FISCO-BCOS/tags | grep -oe "\"name\":\"v[2-9]*\.[0-9]*\.[0-9]*\"" | cut -d \" -f 4 | sort -V | tail -n 1)
+    latest_version=$(curl -sS https://gitee.com/api/v5/repos/FISCO-BCOS/FISCO-BCOS/tags | grep -oe "\"name\":\"v[2-9]*\.[0-9]*\.[0-9]*\"" | grep -v 3. | cut -d \" -f 4 | sort -V | tail -n 1)
     curl -#LO https://github.com/FISCO-BCOS/FISCO-BCOS/releases/download/"${latest_version}"/build_chain.sh && chmod u+x build_chain.sh
     bash build_chain.sh -l 127.0.0.1:4 -o nodes
     cp nodes/127.0.0.1/sdk/* ./
