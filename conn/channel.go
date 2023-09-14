6fbdacc374a9e2c069487f335fc3a9947029b810
liuxinfeng <liuxinfeng96@163.com>
2023-09-14 10:07:00 +0800

    fix: already subscribe to topic

--- conn/channel.go
+++ conn/channel.go
@@ -382,10 +382,10 @@ func DialChannelWithClient(endpoint string, config *tls.Config, groupID int) (*C
 		if err = ch.handshakeChannel(); err != nil {
 			logrus.Errorf("handshake channel protocol failed, use default protocol version")
 		}
-		// ch.topicHandlers[blockNotifyPrefix+strconv.Itoa(groupID)] = nil
-		// if err = ch.sendSubscribedTopics(); err != nil {
-		// 	return nil, fmt.Errorf("subscriber block nofity failed")
-		// }
+		ch.topicHandlers[blockNotifyPrefix+strconv.Itoa(groupID)] = nil
+		if err = ch.sendSubscribedTopics(); err != nil {
+			return nil, fmt.Errorf("subscriber block nofity failed")
+		}
 		return ch, nil
 	})
 }
@@ -752,7 +752,7 @@ func (hc *channelSession) subscribeTopic(topic string, handler func([]byte, *[]b
 	if handler == nil {
 		return errors.New("handler is nil")
 	}
-	if _, ok := hc.topicHandlers[topic]; ok {
+	if oldHandler, ok := hc.topicHandlers[topic]; ok && oldHandler != nil {
 		return errors.New("already subscribed to topic " + topic)
 	}
 	hc.topicMu.Lock()
